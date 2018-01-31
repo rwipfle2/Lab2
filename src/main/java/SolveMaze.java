8cacb2d4c4dd13b95a61304ca1c90e50a307cec5
Rebecca <rwipfle2@illinois.edu>
2018-01-31 16:31:23 -0600

    Maze completed successfully

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,6 +35,19 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while(maze.isFinished() == false){
+                if (maze.canMove() == true) {
+                    maze.move();
+                    maze.turnRight();
+                }
+                else if( maze.canMove() == false){
+                    maze.turnLeft();
+                   }else {
+                       maze.turnLeft();
+                       maze.turnLeft();
+                   }
+                }
+            maze.move();
         }
 
         if (maze.isFinished()) {
